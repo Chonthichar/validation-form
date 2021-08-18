9da660229aa6c2e408006f5be21b9c39fd2e2bed
Chonthichar <chonthichar.soythong@gmail.com>
2021-08-19 00:06:48 +0700

    email validation

--- form.js
+++ form.js
@@ -1,5 +1,19 @@
-function send(){
+
+function send() {
   const emailForm = document.getElementById('email').value;
   const passwordForm = document.getElementById('password').value;
   console.log(emailForm, passwordForm);
-}
+
+  const email = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
+
+  if (!emailForm.match(email)) {
+    alert("Email are in valid");
+    return false;
+  } else if (emailForm.match(email)) {
+    document.getElementById('texts').innerHTML = 'success';
+    return true;}
+  }
+
+
+
+
