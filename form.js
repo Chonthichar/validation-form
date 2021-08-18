8fc5ccdd49be70c12908571a19d63a91969dd7e2
Chonthichar <chonthichar.soythong@gmail.com>
2021-08-18 21:52:24 +0700

    delete doc.element

--- form.js
+++ form.js
@@ -1,7 +1,5 @@
-
 function send(){
   const emailForm = document.getElementById('email').value;
   const passwordForm = document.getElementById('password').value;
-  document.getElementById('email'); console.log(emailForm);
-  document.getElementById('password');console.log(passwordForm);
+  console.log(emailForm, passwordForm);
 }
