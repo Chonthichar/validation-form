f3f6bcbce02ead065865e17b7ff63f5fc8cdbc92
Chonthichar <chonthichar.soythong@gmail.com>
2021-08-19 20:22:12 +0700

    error html and reset button

--- form.js
+++ form.js
@@ -1,4 +1,3 @@
-
 function emails() {
     const emailForm = document.getElementById('email').value;
     const email = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
@@ -17,7 +16,7 @@ function passwords() {
         document.getElementById('message').innerHTML = 'Password length must be between at least 8 characters';
         return false;
 
-    } else if(passwordForm.length > 15){
+    } else if (passwordForm.length > 15) {
         document.getElementById('messages').innerHTML = 'Password length must not over 15 charecters';
         return false;
     } else {
@@ -32,6 +31,7 @@ function send() {
     console.log(emailForm, passwordForm);
 }
 
-function fun() {
+function dataReset() {
     document.getElementById('form').reset();
 }
+
